97ad7695ea81f0efde06ec6539be189521051d9e
asvinod <asvinod@ucsc.edu>
2025-01-24 01:58:14 +0000

    Implemented CRUD functionality for house model

--- affordable-housing/housing/forms.py
+++ affordable-housing/housing/forms.py
@@ -8,13 +8,24 @@ class HouseForm(forms.ModelForm):
     class Meta:
 
         model = House
+        fields = '__all__'
 
-        fields = [
-            'rent', 
-            'beds', 
-            'baths', 
-            'square_feet', 
-            'address', 
-            'description', 
-            'contact',
-            ]
+        lables = {
+            'rent': 'Monthly Rent', 
+            'beds': 'Number Of Bedrooms', 
+            'baths': 'Number of Bathrooms', 
+            'square_feet': 'Sq. Ft', 
+            'address': 'Address',
+            'description': 'Description', 
+            'contact': 'Contact Number',
+        }
+
+        widgets  = {
+            'rent' : forms.NumberInput(attrs={'placeholder': 'eg. 1800'}),
+            'beds' : forms.NumberInput(attrs={'placeholder': 'eg. 1800'}),
+            'baths' : forms.NumberInput(attrs={'placeholder': 'eg. 1800'}),
+            'square_feet' : forms.NumberInput(attrs={'placeholder': 'eg. 1800'}),
+            'address' : forms.TextInput(attrs={'placeholder': 'eg. 1800'}),
+            'description' : forms.Textarea(attrs={'placeholder': 'eg. 1800'}),
+            'contact' : forms.NumberInput(attrs={'placeholder': 'eg. 1800'}),
+        }
