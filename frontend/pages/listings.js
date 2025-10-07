e4ebecb325eeadb8df7db10f00d58df937108b1c
Lamuymuy <123203946+Lamuymuy@users.noreply.github.com>
2025-10-07 14:03:24 -0700

    opshhd

--- frontend/pages/listings.js
+++ frontend/pages/listings.js
@@ -1,3 +1,7 @@
 export default function ListingsPage() {
-  return <p className="bg-green-500">This is the listings page</p>;
+  return (
+    <p className="bg-green-500">
+      This is the listings page !!!!!!!!!!!!!!!!!!!!!!
+    </p>
+  );
 }
