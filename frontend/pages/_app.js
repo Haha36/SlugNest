e70193df1d7fa2b059091ba3361bcce88af85a46
Ubuntu <ubuntu@ip-172-31-26-151.us-east-2.compute.internal>
2026-02-17 05:46:40 +0000

    Add favicon

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,17 @@
 import Navbar from "../components/Navbar";
 import "../styles/global.css";
 import { AuthProvider } from "../contexts/AuthContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </AuthProvider>
