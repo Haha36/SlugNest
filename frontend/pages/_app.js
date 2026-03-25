182287ed1ab012b5515717da1844b88adfd22b4c
Yifei Peng <ypeng64@ucsc.edu>
2026-03-24 18:41:41 -0700

    Added Google tag

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -11,6 +11,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F1B1FS6NXJ"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-F1B1FS6NXJ');
+        `}} />
       </Head>
       <Navbar />
       <Component {...pageProps} />
