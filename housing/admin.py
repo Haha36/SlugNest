e68c7411b20e02434779f2d56664e5fe1d4fe9ce
Yifei Peng <ypeng64@ucsc.edu>
2026-05-26 17:19:34 -0700

    Add owner and listing_type fields to Django admin

--- housing/admin.py
+++ housing/admin.py
@@ -3,4 +3,4 @@ from .models import House
 
 @admin.register(House)
 class HousingAdmin(admin.ModelAdmin):
-    fields = ['rent', 'beds', 'baths', 'square_feet', 'address', 'description', 'contact', 'More_information']
+    fields = ['owner', 'listing_type', 'rent', 'beds', 'baths', 'square_feet', 'address', 'description', 'contact', 'More_information']
